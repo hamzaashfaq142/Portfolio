952d115f31b0e187bbfc15aee1d87646307a2416
Hamza Ashfaq <hamzahazi413@gmail.com>
2023-10-17 16:24:54 +0500

    *push

--- src/config.js
+++ src/config.js
@@ -2,32 +2,32 @@ var Config = {
     navHeader : "Portfolio",
     portfolioName: "Hamza Ishfaq.",
     tagLine: "Empowering businesses through data-driven solutions.",
-    overview: "I am an experienced SQL Developer with expertise in BI, data analytics, and data visualization. Skilled in Node JS, React JS, MongoDB, and Express JS. Proven track record of success in data modeling, ETL, dashboarding, and API development. Passionate about harnessing the power of data to drive growth and innovation. Let's connect to take your business to the next level.",
-    resumeLink : "https://1drv.ms/b/s!AjVMrCd_e_C_g_tTXQdLjV4WDyVIQg?e=JJJUGj",
-    email : "daniyalriaz371@gmail.com",
-    about : "Hi, my name is Muhammad Daniyal and I am a passionate creator who loves to solve problems. My areas of interest include Databases, Data Visualization, Data Analytics and Web Development. I began my career as a Database Developer when I landed an internship at a company after completing my intermediate studies. I discovered that I had a natural talent for learning and practicing SQL and programming languages.<br/><br/>Since then, I have gained valuable experience working in the retail industry and freelancing for various clients. My current focus is on mastering modern technologies and creating accessible and inclusive digital experiences. <br/><br/>I believe in constantly learning and expanding my knowledge base to stay up-to-date with the latest trends and technologies. I am excited to continue my journey as a creator and look forward to taking on new challenges and opportunities. <br/><br/>Some of the technologies I have been working with recently include:",
-    imgPath : "img/portrait.JPG",
-    techExperience : ['Databases','SQL','PL/SQL','Data Visulization','ETL','HTML/CSS','Javascript','Node Js','React Js','MongoDB','Express','REST APIs'],
+    overview: "I am an experienced Frontend Developer with expertise in Angular and React js. Skilled in Node JS, React JS, Sql, and Express JS. Proven track record of success in Frontend dashboarding, and API development. Passionate about harnessing the power of data to drive growth and innovation. Let's connect to take your business to the next level.",
+    resumeLink : "https://drive.google.com/file/d/1OUzPxY1ByBkHyuovcrRDicJ-6s1iTmpi/view?usp=sharing",
+    email : "hamzy.ashfaq@gmail.com",
+    about : "Hi, my name is Hamza Ishfaq and I am a passionate creator who loves to solve problems. My areas of interest include Frontend, Data Analytics and Web Development. I began my career as a Database Developer when I landed an internship at a company after completing my graduation. I discovered that I had a natural talent for learning and practicing Frontend frameworks and programming languages.<br/><br/>Since then, I have gained valuable experience working in the retail industry and freelancing for various clients. My current focus is on mastering modern technologies and creating accessible and inclusive digital experiences. <br/><br/>I believe in constantly learning and expanding my knowledge base to stay up-to-date with the latest trends and technologies. I am excited to continue my journey as a creator and look forward to taking on new challenges and opportunities. <br/><br/>Some of the technologies I have been working with recently include:",
+    imgPath : "img/hamzy.jpg",
+    techExperience : ['Angular','React Js','SQL','HTML/CSS','Javascript','Node Js','Express','REST APIs'],
     noOfTechColumns : 3,
     jobs : [
         {
             companyShortName : "CSP",
             link : "https://cs.com.pk/",
             companyLongName : "Computing Solutions Pvt. Ltd",
-            designation : "Database Developer",
-            period : "Sep 2020 - Feb 2023",
+            designation : "Associate Software Developer",
+            period : "Feb 2023 - Oct 2023",
             responsibilities : [
                 {
-                    data : "Database Developer at Computing Solutions Pvt. Ltd, a leading Retail Industry Company."
+                    data : "Associate Software Developer at Computing Solutions Pvt. Ltd, a leading Retail Industry Company."
                 },
                 {
-                    data : "Developed complex queries and database objects including procedures, tables, and views."
+                    data : "User Interface Development: I excel in creating intuitive and user-friendly interfaces using Angular and React. I can leverage my skills to design and implement the frontend components that interact with the complex queries and database objects."
                 },
                 {
-                    data : "Utilized reporting tools such as Jasper Reports, SAP Crystal Reports, Power BI, Targit BI, and Krunch to visualize data and presented solutions to clients after gathering their requirements."
+                    data : "API Integration: I can seamlessly integrate the frontend with back-end services and APIs, ensuring that data is fetched and displayed efficiently. This will make the procedures, tables, and views easily accessible to the end users."
                 },
                 {
-                    data : "Assisted with Retail Pro ERP and Canias ERP, modifying reports, dashboards, forms, and dialogs to meet client needs."
+                    data : "Testing and Debugging: Thorough testing and debugging are essential in the development process. I can implement unit testing and debugging procedures to ensure the reliability and robustness of the application."
                 }
             ]
         }
@@ -35,22 +35,22 @@ var Config = {
     socials : [
         {
             icon : "facebook",
-            href : "https://www.facebook.com/profile.php?id=100011717698040",
+            href : "https://www.facebook.com/hamza.ashfaq143?mibextid=ZbWKwL",
             target : "_blank"
         },
         {
             icon : "whatsapp",
-            href : "https://wa.me/+923181445347",
+            href : "https://wa.me/+923001010423",
             target : "_blank"
         },
         {
             icon : "github",
-            href : "https://github.com/DaniMughal371",
+            href : "https://github.com/hamzaashfaq142",
             target : "_blank"
         },
         {
             icon : "linkedin",
-            href : "https://www.linkedin.com/in/muhammad-daniyal-a325a31a1/",
+            href : "www.linkedin.com/in/hamza-ashfaq-773a1a199",
             target : "_blank"
         }
     ],
@@ -75,7 +75,7 @@ var Config = {
         },
         {
             name : 'Resume',
-            href : 'https://1drv.ms/b/s!AjVMrCd_e_C_hIR2Jn4CP3WXgUR4oA?e=oewJYh',
+            href : 'https://drive.google.com/file/d/1OUzPxY1ByBkHyuovcrRDicJ-6s1iTmpi/view?usp=sharing',
             class : 'btn rounded-3 p-2',
             target : "_blank"
         }
